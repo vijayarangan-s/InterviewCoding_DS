35d6c20fbeb14e81f58b3db7c8f50ec82a1a76de
vijayarangan <75248380+vijayarangan-s@users.noreply.github.com>
2021-05-27 14:12:01 +0530

    Update rotateMatrixArray.js

--- rotateMatrixArray.js
+++ rotateMatrixArray.js
@@ -15,6 +15,23 @@ var transposeArr = (matrix) => {
     return result;
 }
 
+var transposeArr = (matrix) => { // 2nd method
+    if(matrix.length === 0) return []
+
+    let len = matrix[0].length,newArr = []
+
+    for(let i=0; i<len; i++){
+        let result = []
+        for(let j=0; j<len; j++){
+
+            let row = matrix[j][i];
+            result.push(row)
+        }
+        newArr.push(result.reverse())
+    }
+    return newArr;
+}
+
 transposeArr([
   [1,2,3],
   [4,5,6], 
